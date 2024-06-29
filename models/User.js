e6545ca03122a03b016360b75fb9aa976f93d3d0
NadifRahman <nadif.rah@gmail.com>
2024-06-29 16:24:53 -0400

    Create user model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+/*
+- First name and last name are required and max length of 20
+- Usernmame has max length of 15
+- Hashed password has no limit but is required, will use other validation on register to limit character count
+*/
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 20 },
+  last_name: { type: String, required: true, maxLength: 20 },
+  username: { type: String, required: true, maxLength: 15 },
+  hashedPassword: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
