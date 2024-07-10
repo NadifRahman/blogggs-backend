e8c686cb7fb7bcbfb4755e3948ebc2975148f705
NadifRahman <nadif.rah@gmail.com>
2024-07-09 20:37:42 -0400

    Prepare for production

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+//Limiter
+//Limiter
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 1000,
+  message: 'Too many requests, please wait',
+}); //1 minute window, max 1000 requests per min
+app.use(limiter);
 
 //CONFIG THE DATABASE
 require('./config/database.js');
@@ -28,6 +40,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -55,15 +69,6 @@ app.use(function (err, req, res, next) {
     responseToJson.err = err;
   }
   res.json(responseToJson);
-
-  /* delete this after, from generator
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-  */
 });
 
 module.exports = app;
